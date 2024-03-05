5c7b3dc8aa829acb3bbf1e20cf46717c2acd329b
Tawanda3012 <tawanda@uncommon.org>
2024-03-05 12:01:35 +0200

    Hero-section

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './HeroSection.css';
+import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
     <section className="hero-section">
-      {/* Hero section content */}
+      <div className="hero-section-text">
+        <h1>Join the uncommon way to hire</h1>
+        <p>
+          At Uncommon, we train unemployed young adults surviving on less than
+          $5 per day for the technology workforce.
+        </p>
+        <p>We can change lives, one job at a time.</p>
+      </div>
+
+      <div className="hero-section-image">
+        <img src="../../Assets/Images/Frame 114 (1).png" alt="Uncommon Hero Section" />
+      </div>
     </section>
   );
 };
