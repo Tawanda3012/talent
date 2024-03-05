60a628d4aa4288c1ad607814e874d9d72204b154
Tawanda3012 <tawanda@uncommon.org>
2024-03-05 15:55:02 +0200

    ProfileGrid section

--- src/Components/ProfileGrid/ProfileGrid.js
+++ src/Components/ProfileGrid/ProfileGrid.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import profilesData from './profiles.json';
 
-function ProfileGrid() {
+const ProfileGrid = () => {
   return (
-    <div>ProfileGrid</div>
-  )
+    <div className="container">
+      <div className="row">
+        {profilesData.map((profile, index) => (
+          <div key={index} className="col-md-4">
+            <div className="card">
+              <img src={profile.image} className="card-img-top" alt={profile.name} />
+              <div className="card-body">
+                <h5 className="card-title">{profile.name}</h5>
+                <p className="card-text">{profile.role}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default ProfileGrid
+export default ProfileGrid;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import HeroSection from "../Components/HeroSection/HeroSection"
+import ProfileGrid from '../Components/ProfileGrid/ProfileGrid'
 
 export default function Home() {
   return (
     <div>
        <Header/>
        <HeroSection/>
+       <ProfileGrid/>
     </div>
   )
 }
