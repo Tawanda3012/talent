e4eb48d813abf56b1cf38031da3b5e95805c011f
Tawanda3012 <tawanda@uncommon.org>
2024-03-06 12:45:48 +0200

    ProfileGrid

--- src/Components/ProfileGrid/ProfileGrid.js
+++ src/Components/ProfileGrid/ProfileGrid.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import profilesData from './profiles.json';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import profilesData from "../../Data/profiles.json";
 
 const ProfileGrid = () => {
+  const [fetchedProfilesData, setFetchedProfilesData] = useState([]);
+
+  useEffect(() => {
+    setFetchedProfilesData(profilesData); // Use the imported profilesData directly
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
-        {profilesData.map((profile, index) => (
-          <div key={index} className="col-md-4">
+        {fetchedProfilesData.map((profile, index) => (
+          <div key={index} className="col-md-3 mb-4 mt-5">
             <div className="card">
-              <img src={profile.image} className="card-img-top" alt={profile.name} />
+              <img
+                src={profile.image}
+                className="card-img-top"
+                alt={`Image ${profile.id}`}
+                style={{ height: "200px", width: "100%" }}
+              />
               <div className="card-body">
                 <h5 className="card-title">{profile.name}</h5>
                 <p className="card-text">{profile.role}</p>
@@ -20,6 +32,6 @@ const ProfileGrid = () => {
       </div>
     </div>
   );
-}
+};
 
 export default ProfileGrid;
