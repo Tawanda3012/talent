a16400ffd5151c9896ff69176aba25cca421df85
Tawanda3012 <tawanda@uncommon.org>
2024-03-07 09:28:39 +0200

    new

--- src/Components/ProfileGrid/ProfileGrid.js
+++ src/Components/ProfileGrid/ProfileGrid.js
@@ -1,1 +1,40 @@
- 
+ /* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import profilesData from "../../Data/profiles.json"
+
+const ProfileGrid = () => {
+  const [fetchedProfilesData, setFetchedProfilesData] = useState([]);
+
+  useEffect(() => {
+    setFetchedProfilesData(profilesData); // Use the imported profilesData directly
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        {fetchedProfilesData.map((profile, index) => (
+          <div key={index} className="col-md-3 mb-4 mt-5">
+            <Link to={`/profile/${profile.id}`} style={{ textDecoration: 'none' }}>
+              <div className="card">
+                <img
+                  src={profile.image}
+                  className="card-img-top"
+                  alt={`Image ${profile.id}`}
+                  style={{ height: "200px", width: "100%" }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{profile.name}</h5>
+                  <p className="card-text">{profile.role}</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileGrid;
